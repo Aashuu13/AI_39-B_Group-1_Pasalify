87d6e54f4b735aef62e5c6358eff0dc57daca101
Aashuu13 <bastakotiaashutosh10@gmail.com>
2026-05-12 12:13:22 +0545

    commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def about():
-    return "Pasalify Welcomes You! Happy to have you here"
+    return "Pasalify Welcomes You! Happy to have you here 😍"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
